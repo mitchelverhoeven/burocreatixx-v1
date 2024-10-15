c7f7ea19a30b4baccc0e5c0f2daba824320c7dfc
mitchelverhoeven <info@werkgever-gezocht.nl>
2024-10-15 10:28:15 +0200

    ons verhaal margin top aangepast

--- src/app/_components/Pages/OurStoryPage/ContentSection.tsx
+++ src/app/_components/Pages/OurStoryPage/ContentSection.tsx
@@ -9,7 +9,7 @@ export default function ContentSection() {
     <div className="mb-[300px]">
       <div
         className="grid grid-cols-2 w-full h-[400px] max-[1050px]:grid-cols-1
-      max-[1050px]:-mt-[200px] max-[425px]:-mt-[100px]"
+      max-[1050px]:-mt-[200px]"
       >
         <div className="max-[1050px]:order-2">
           <ContentSectionLeft />

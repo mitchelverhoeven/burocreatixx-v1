b7f5281431bce2ad50d69f786d8c237942e06320
mitchelverhoeven <info@werkgever-gezocht.nl>
2024-11-10 13:37:44 +0100

    Meta Pixel toegevoegd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,8 @@ import CookieConsent from "react-cookie-consent";
 
 import NavBar from "./_components/NavBar/NavBar";
 import StructuredData from "./_components/StructuredData/StructuredData";
+import Head from "next/head";
+import Image from "next/image";
 
 const dateMetadata = {
   published: new Date("2024-10-30").toISOString(),
@@ -24,6 +26,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
+      <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '596533896043721'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=596533896043721&ev=PageView&noscript=1"
+            alt=""
+          ></Image>
+        </noscript>
+      </Head>
       <body>
         <Link
           href="https://db.onlinewebfonts.com/c/7a47ed05475430352e621c843953e9f9?family=Chakra+Petch+Bold"
